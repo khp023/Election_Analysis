5c2dad8778228a11362c337bd3b21367ae6ad134
Khanh <khphan183@gmail.com>
2022-12-07 22:50:27 -0800

    Ready for submit

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -115,14 +115,14 @@ with open(file_to_save, "w") as txt_file:
         f"Largest County Turnout: {countyLargestTurnout}\n"
         f"-------------------------\n"
     )
-    print(LargestCountyTurnout)
-    txt_file.write(LargestCountyTurnout)
+    
+    
 
     # 7: Print the county with the largest turnout to the terminal.
-
+    print(LargestCountyTurnout)
 
     # 8: Save the county with the largest turnout to a text file.
-
+    txt_file.write(LargestCountyTurnout)
 
     # Save the final candidate vote count to the text file.
     for candidate_name in candidate_votes:
